9896b4b472d3394697e1dd16bbe7d6260a022498
ElectroSpyder <prg@live.com.ar>
2015-12-07 18:29:41 -0300

    Primer Comit de la aplicacion

---
+++ Empresa.Entity/Almacenamiento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa.Entity
+{
+    public class Almacenamiento
+    {
+        public virtual Guid Id { get; set; }
+
+    }
+}
---
+++ Empresa.Entity/Empleado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa.Entity
+{
+    public class Empleado
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Nombre { get; set; }
+        public virtual string Apellido { get; set; }
+        public virtual Almacenamiento Almacenamiento { get; set; }
+    }
+}
